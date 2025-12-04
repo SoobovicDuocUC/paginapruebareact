6d126ff1b29cc1764bff75cef2e571d6186ac0a5
SoobovicDuocUC <seba.arayar@duocuc.cl>
2025-12-04 02:45:16 -0300

    Usuario service arreglado

--- src/components/services/UsuarioService.js
+++ src/components/services/UsuarioService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/usuarios';
+const BASE_URL = 'http://localhost:8080/api/auth'; 
 
 const usuarioService = {
   login: (email, password) => {
     return axios.post(`${BASE_URL}/login`, { email, password });
   },
-  register: (usuario) => {
+  register: (usuario) => { // usuario es { email, password, role }
     return axios.post(`${BASE_URL}/register`, usuario);
   }
 };
